b3769c911495f191fc4b9bbf1ed73cad62f76512
scaminati <simone.caminati@gmail.com>
2025-08-27 16:59:26 +0200

    deploy: import fix

--- fantamd-be/api/index.js
+++ fantamd-be/api/index.js
@@ -1,7 +1,7 @@
 const { initFastify } = require('../server.js')
 const app = initFastify()
 
-module.exports = async function handler (req, res) {
+export default async function handler (req, res) {
   await app.ready()
   app.server.emit('request', req, res)
 }
