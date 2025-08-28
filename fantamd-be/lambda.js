0ceb359eaffbbd063e5a60c6fc5f07a8ef3bcbfb
scaminati <simone.caminati@gmail.com>
2025-08-28 18:16:51 +0200

    fix: netlify config

--- fantamd-be/lambda.js
+++ fantamd-be/lambda.js
@@ -1,4 +1,5 @@
 import awsLambdaFastify from '@fastify/aws-lambda'
 import app from './app.js'
-export const handler = awsLambdaFastify(app)
-await app.ready() // needs to be placed after awsLambdaFastify call because of the decoration: https://github.com/fastify/aws-lambda-fastify/blob/main/index.js#L9
+export const handler = awsLambdaFastify(app, {
+  decorateRequest: false
+})
