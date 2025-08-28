4cd8a4ac78ab17fbf1a1e1365c6f9ee47b07f236
scaminati <simone.caminati@gmail.com>
2025-08-28 15:19:11 +0200

    deploy: test dist

---
+++ fantamd-be/api/index.js
@@ -0,0 +1,8 @@
+import { initFastify } from '../server.js'
+
+const app = initFastify()
+
+export default async function handler (req, res) {
+  await app.ready()
+  app.server.emit('request', req, res)
+}
