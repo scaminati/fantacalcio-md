10359f99a7237f590d97a1df956e0b8b89888dee
scaminati <simone.caminati@gmail.com>
2025-09-16 17:48:10 +0200

    test: theme switch test

---
+++ fantamd-fe/test/components/theme-switch.test.tsx
@@ -0,0 +1,19 @@
+import { expect, test } from "vitest";
+import { fireEvent, render, screen } from "@testing-library/react";
+import { ThemeProvider } from "next-themes";
+
+import { ThemeSwitch } from "@/components/theme-switch";
+
+test("ThemeSwitch component", async () => {
+  render(
+    <ThemeProvider attribute="class">
+      <ThemeSwitch />
+    </ThemeProvider>,
+  );
+
+  expect(await screen.findByTestId("moon-icon")).toBeDefined();
+
+  fireEvent.click(await screen.findByTestId("switch-btn"));
+
+  expect(await screen.findByTestId("sun-icon")).toBeDefined();
+});
--- fantamd-fe/test/test-setup.ts
+++ fantamd-fe/test/test-setup.ts
@@ -14,3 +14,17 @@ afterEach(() => {
   vi.clearAllMocks();
   cleanup();
 });
+
+Object.defineProperty(window, "matchMedia", {
+  writable: true,
+  value: vi.fn().mockImplementation((query) => ({
+    matches: true,
+    media: query,
+    onchange: null,
+    addListener: vi.fn(), // deprecated
+    removeListener: vi.fn(), // deprecated
+    addEventListener: vi.fn(),
+    removeEventListener: vi.fn(),
+    dispatchEvent: vi.fn(),
+  })),
+});
